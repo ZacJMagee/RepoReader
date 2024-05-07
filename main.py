9a6996ff0a0b95542b476a1d0b2c0b96d72b681d
IntiMech <zacmconsulting@gmail.com>
2024-05-07 14:45:03 +1000

    Working with autogen repo

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import os
 import tempfile
-import re
-import nltk
+import uuid
 import uuid
 import subprocess
 from dotenv import load_dotenv
+from langchain.indexes import SQLRecordManager, index
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.documents import Document
 from langchain_community.document_loaders import DirectoryLoader, NotebookLoader, PythonLoader, TextLoader
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain.storage import InMemoryStore
 from langchain.retrievers import ParentDocumentRetriever
-from rank_bm25 import BM25Okapi
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -21,8 +21,6 @@ WHITE = "\033[37m"
 GREEN = "\033[32m"
 RESET_COLOR = "\033[0m"
 model_name = "gpt-3.5-turbo"
-
-nltk.download("punkt")
 
 class QuestionContext:
     def __init__(self, index, documents, llm_chain, model_name, repo_name, github_url, conversation_history, file_type_counts, filenames):
@@ -36,21 +34,33 @@ class QuestionContext:
         self.file_type_counts = file_type_counts
         self.filenames = filenames
 
+
+class DocumentIndexer:
+    def __init__(self, vector_store, record_manager):
+        self.vector_store = vector_store
+        self.record_manager = record_manager
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+    def index_documents(self, documents, cleanup_mode="incremental"):
+        split_documents = self.split_and_prepare(documents)
+        index(split_documents, self.record_manager, self.vector_store, cleanup=cleanup_mode, source_id_key="source")
+
+    
+    def split_and_prepare(self, documents):
+        split_documents = []
+        for original_doc in documents:
+            file_id = original_doc.metadata['file_id']  # Assuming 'file_id' is stored in metadata
+            split_docs = self.text_splitter.split_documents([original_doc])
+            for split_doc in split_docs:
+                split_doc.metadata['file_id'] = file_id  # Ensure file ID is maintained
+                split_doc.metadata['source'] = original_doc.metadata['source']
+            split_documents.extend(split_docs)
+        return split_documents
+
 class RepositoryManager:
-    def __init__(self):
-        load_dotenv()
-        self.OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-    def clone_github_repo(self, github_url, local_path):
-        try:
-            subprocess.run(['git', 'clone', github_url, local_path], check=True)
-            return True
-        except subprocess.CalledProcessError as e:
-            print(f"Failed to clone repository: {e}")
-            return False
-
-    def load_and_index_files(self, repo_path):
-        extensions = {
+    def __init__(self, repo_path):
+        self.repo_path = repo_path
+        self.extensions = {
             'txt': TextLoader,
             'md': TextLoader,
             'markdown': TextLoader,
@@ -80,65 +90,37 @@ class RepositoryManager:
             'css': TextLoader,
             'scss': TextLoader,
             'sql': TextLoader,
-            'editorconfig': TextLoader,
             'ipynb': NotebookLoader  # Special case for Jupyter notebooks
         }
 
-        file_type_counts = {}
-        documents_dict = {}
+    def clone_github_repo(self, github_url, local_path):
+        try:
+            subprocess.run(['git', 'clone', github_url, local_path], check=True)
+            return True
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to clone repository: {e}")
+            return False
 
-        for ext, LoaderClass in extensions.items():
-            glob_pattern = f'**/*.{ext}'
-            loader_kwargs = {'autodetect_encoding': True} if LoaderClass is TextLoader else {}
+    def load_documents(self):
+        documents = []
+        for ext, LoaderClass in self.extensions.items():
+            loader = DirectoryLoader(
+                self.repo_path,
+                glob=f"**/*.{ext}",
+                loader_cls=LoaderClass,
+                loader_kwargs={'autodetect_encoding': True},
+                use_multithreading=True,
+                show_progress=True
+            )
             try:
-                if LoaderClass is NotebookLoader:
-                    loader = LoaderClass(repo_path, include_outputs=True, max_output_length=20, remove_newline=True)
-                else:
-                    loader = DirectoryLoader(repo_path, glob=glob_pattern, loader_cls=LoaderClass, loader_kwargs=loader_kwargs, silent_errors=True)
-
-                loaded_documents = loader.load()
-                if loaded_documents:
-                    file_type_counts[ext] = len(loaded_documents)
-                    for doc in loaded_documents:
-                        file_path = doc.metadata['source']
-                        relative_path = os.path.relpath(file_path, repo_path)
-                        file_id = str(uuid.uuid4())
-                        doc.metadata['source'] = relative_path
-                        doc.metadata['file_id'] = file_id
-
-                        documents_dict[file_id] = doc
+                docs = loader.load()
+                for doc in docs:
+                    doc_id = str(uuid.uuid4())
+                    doc.metadata['file_id'] = doc_id
+                    documents.append(doc)
             except Exception as e:
-                print(f"Error loading files with pattern '{glob_pattern}': {e}")
-                continue
-
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=200)
-
-        split_documents = []
-        for file_id, original_doc in documents_dict.items():
-            split_docs = text_splitter.split_documents([original_doc])
-            for split_doc in split_docs:
-                split_doc.metadata['file_id'] = original_doc.metadata['file_id']
-                split_doc.metadata['source'] = original_doc.metadata['source']
-
-            split_documents.extend(split_docs)
-
-        index = None
-        if split_documents:
-            tokenized_documents = [self.clean_and_tokenize(doc.page_content) for doc in split_documents]
-            index = BM25Okapi(tokenized_documents)
-        return index, split_documents, file_type_counts, [doc.metadata['source'] for doc in split_documents]
-
-
-    def clean_and_tokenize(self, text):
-        text = re.sub(r'\s+', ' ', text)
-        text = re.sub(r'<[^>]*>', '', text)
-        text = re.sub(r'\[.*?\]', '', text)
-        text = re.sub(r'\(.*?\)', '', text)
-        text = re.sub(r'\b(?:http|ftp)s?://\S+', '', text)
-        text = re.sub(r'\W', ' ', text)
-        text = re.sub(r'\d+', '', text)
-        text = text.lower()
-        return nltk.word_tokenize(text)
+                print(f"Failed to load documents for {ext}: {e}")
+        return documents
 
 
 class EnhancedDocumentRetriever:
@@ -155,7 +137,8 @@ class EnhancedDocumentRetriever:
             vectorstore=self.vectorstore,
             docstore=self.store,
             child_splitter=self.child_splitter,
-            parent_splitter=self.parent_splitter
+            parent_splitter=self.parent_splitter,
+            max_documents_per_query=5  # Limiting the number of documents returned per query
         )
 
     def add_documents(self, documents):
@@ -168,7 +151,6 @@ class SearchAndQueryManager:
     """Manages search and querying of documents, using EnhancedDocumentRetriever for document retrieval."""
     def __init__(self, document_retriever):
         self.document_retriever = document_retriever
-        nltk.download("punkt")
 
     def search_documents(self, query, n_results=5):
         retrieved_docs = self.document_retriever.retrieve_documents(query)
@@ -181,54 +163,85 @@ class SearchAndQueryManager:
 
 class ApplicationController:
     def __init__(self):
-        self.repository_manager = RepositoryManager()
+        # This no longer takes repo_path as a parameter at initialization.
+        self.repository_manager = None
 
     def main(self):
         github_url = input(WHITE + "Enter the GitHub URL of the repository: " + RESET_COLOR)
         repo_name = github_url.split("/")[-1]
         print(WHITE + "Cloning the repository..." + RESET_COLOR)
-        
+
         with tempfile.TemporaryDirectory() as local_path:
+            self.repository_manager = RepositoryManager(local_path)
             if self.repository_manager.clone_github_repo(github_url, local_path):
-                print("Repository cloned. Indexing files...")
-                index, documents, file_type_counts, filenames = self.repository_manager.load_and_index_files(local_path)
-                
+                print("Repository cloned. Loading and indexing files...")
+                documents = self.repository_manager.load_documents()
+
                 if not documents:
                     print("No documents were found to index. Exiting.")
-                    exit()
+                    return
 
-                # Create the document retriever instance
-                document_retriever = EnhancedDocumentRetriever(collection_name=repo_name)
-                document_retriever.add_documents(documents)
-                self.search_and_query_manager = SearchAndQueryManager(document_retriever)
-
-                # Prepare the chat template
-                file_type_counts_str = ', '.join(f"{k}: {v}" for k, v in file_type_counts.items())
-                filenames_str = ', '.join(filenames)
-                prompt = ChatPromptTemplate.from_messages([
-                    ("system", f"You are a helpful assistant for analyzing code repositories. Repository Name: {repo_name}, GitHub URL: {github_url}, "
-                               f"Document Count: {len(documents)}, File Type Counts: {file_type_counts_str}, File Names: {filenames_str}"),
-                    ("human", "{user_question}")
-                ])
-
-                llm = ChatOpenAI(model=model_name, temperature=0)
-                chain = prompt | llm
-                conversation_history = ""
-                while True:
-                    user_question = input(WHITE + "\nAsk a question about the repository (type 'exit()' to quit): " + RESET_COLOR)
-                    if user_question.lower() == 'exit()':
-                        break
-                    print(WHITE + 'Thinking...' + RESET_COLOR)
-                    # Search documents using the question
-                    retrieved_docs = self.search_and_query_manager.search_documents(user_question, n_results=5)
-                    formatted_docs = self.search_and_query_manager.format_documents(retrieved_docs)
-                    print(GREEN + f"\nRETRIEVED DOCS\n{formatted_docs}" + RESET_COLOR)
-                    
-                    # Continue the conversation using the model
-                    full_response = chain.invoke({"user_question": user_question})
-                    content = full_response.content
-                    token_usage = full_response.response_metadata['token_usage']
-                    print(GREEN + f'\nFULL RESPONSE\nContent: {content}\nToken Usage: Completion: {token_usage["completion_tokens"]}, Prompt: {token_usage["prompt_tokens"]}, Total: {token_usage["total_tokens"]}' + RESET_COLOR)
-                    conversation_history += f"Question: {user_question}\nFull Response: Content: {content}, Token Usage: {token_usage}\n"
+                document_retriever = self.setup_document_retriever(documents, repo_name)
+                self.interactive_query(repo_name, document_retriever)
             else:
                 print("Failed to clone the repository.")
+
+    def setup_document_retriever(self, documents, repo_name):
+        # Setup vector store and record manager
+        vector_store = Chroma(collection_name=repo_name, embedding_function=OpenAIEmbeddings(model="text-embedding-3-large", dimensions=3072))
+        record_manager = SQLRecordManager(namespace=f"{repo_name}_namespace", db_url="sqlite:///my_index.db")
+        record_manager.create_schema()
+
+        # Index documents
+        document_indexer = DocumentIndexer(vector_store, record_manager)
+        document_indexer.index_documents(documents, cleanup_mode="incremental")
+
+        # Setup document retriever
+        document_retriever = EnhancedDocumentRetriever(collection_name=repo_name, dimensions=3072)
+        document_retriever.add_documents(documents)
+
+        return document_retriever
+
+    
+    
+    def interactive_query(self, repo_name, document_retriever):
+        # Define the system's initial prompt/message
+        system_prompt = f"You are a helpful assistant for analyzing the repository: {repo_name}. Ask me anything about it."
+        
+        # Initialize the llm model
+        llm = ChatOpenAI(model=model_name, temperature=0)
+        
+        while True:
+            user_question = input(WHITE + "\nAsk a question about the repository (type 'exit()' to quit): " + RESET_COLOR)
+            if user_question.lower() == 'exit()':
+                break
+            print(WHITE + 'Thinking...' + RESET_COLOR)
+            
+            # Retrieve document chunks relevant to the query if necessary
+            retrieved_docs = document_retriever.retrieve_documents(user_question)
+            context = " ".join([doc.page_content for doc in retrieved_docs])  # Concatenate content for context
+
+            # Create messages as per the required format
+            messages = [
+                ("system", system_prompt),
+                ("human", user_question)
+            ]
+
+            # If there's additional context from documents, add it as another system message
+            if context:
+                messages.insert(1, ("system", context))
+
+            # Invoke the model with the formatted messages
+            response = llm.invoke(messages)
+            
+            # Output the response content
+            print(GREEN + f'\nFULL RESPONSE\nContent: {response.content}' + RESET_COLOR)
+
+    def process_query(self, prompt, llm, user_question):
+        # Here, we need to invoke the chain correctly
+        full_response = prompt.invoke({"user_question": user_question}) | llm
+        print(GREEN + f'\nFULL RESPONSE\nContent: {full_response.content}' + RESET_COLOR)
+
+if __name__ == "__main__":
+    app_controller = ApplicationController()
+    app_controller.main()
